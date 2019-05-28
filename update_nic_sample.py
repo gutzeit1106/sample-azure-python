7a0f1de048ba76cbfc3b132ae383372cbae80263
gutzeit1106 <gutzeit.11.06@gmail.com>
2019-05-28 13:54:04 +0000

    remain the previous settings except private ip

--- update_nic_sample.py
+++ update_nic_sample.py
@@ -5,13 +5,9 @@ from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.network import NetworkManagementClient
 
 #Parameter
-LOCATION = 'japaneast'
 GROUP_NAME = 'MyResourceGroup'
-IP_CONFIG_NAME = 'ipconfig1'
-NIC_NAME = 'MyNIC'
-PRIVATE_IPADDRESS = '10.0.0.12'
-SUBNET_ID = '/subscriptions/xxxx/resourceGroups/MyResourceGroup/providers/Microsoft.Network/virtualNetworks/MyVnet/subnets/Mysubnet'
-
+NIC_NAME = 'ipconfig1'
+PRIVATE_IPADDRESS = '192.168.10.14'
 
 def get_credentials():
     subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
@@ -29,25 +25,17 @@ def run_example():
     credentials, subscription_id = get_credentials()
     network_client = NetworkManagementClient(credentials, subscription_id)
 
+    # Get NetworkInterface by name
+    print('Get Current NetworkInterface by Name')
+    network_interface =  network_client.network_interfaces.get(GROUP_NAME, NIC_NAME)
     #Update Network Interface
+    network_interface.ip_configurations[0].private_ip_address = PRIVATE_IPADDRESS 
+    network_interface.ip_configurations[0].private_ip_allocation_method = "static"
     print('Update NIC')
     async_nic_creation = network_client.network_interfaces.create_or_update(
         GROUP_NAME,
         NIC_NAME,
-        {
-            'location': LOCATION,
-            'ip_configurations': [{
-                'name': IP_CONFIG_NAME,
-                "properties": {
-                    "privateIPAddress": PRIVATE_IPADDRESS,
-                    "privateIPAllocationMethod": "Static",
-                     'subnet': {
-                        'id': SUBNET_ID
-                    }
-                    }
-                }
-            ]
-        }
+        network_interface
     )
 
 if __name__ == "__main__":
